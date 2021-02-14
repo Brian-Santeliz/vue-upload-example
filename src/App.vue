4c97a3fb90164cbed8ba6754924dca87e2ee3810
Brian <blsvsanteliz@gmail.com>
2021-02-13 23:58:45 -0400

    add input with ref

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,43 @@
 <template>
   <div>
     <h1>Agrega imagenes</h1>
-    <input type="file" name="" id="" @change="addChange" />
-    <button type="submit" @click="subir" :disabled="disabled">Subit</button>
+    <!-- <input type="file" name="" id="" @change="addChange" /> -->
+    <input
+      type="file"
+      name=""
+      id=""
+      ref="inputFiles"
+      @change="addMultiple"
+      multiple
+    />
+    <button type="submit" @click="subir" :disabled="disabled">
+      Submit Single
+    </button>
+    <button type="submit" @click="subirArray" :disabled="disabled">
+      Submit ARRAY
+    </button>
   </div>
 </template>
 
 <script>
 import uploadImage from "../graphql/uploadImage.graphql";
+import uploadArrayImages from "../graphql/uploadArrayImages.graphql";
 export default {
   name: "App",
   data() {
     return {
       file: null,
       disabled: false,
+      files: [],
     };
   },
   methods: {
     addChange({ target }) {
       this.file = target.files[0];
+    },
+    addMultiple() {
+      this.files = this.$refs.inputFiles.files;
+      // e.target.files
     },
     subir() {
       if (!this.file) return;
@@ -40,6 +59,18 @@ export default {
           }
         );
       this.disabled = false;
+    },
+    subirArray() {
+      this.$apollo
+        .mutate({
+          mutation: uploadArrayImages,
+          variables: {
+            files: this.files,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+        });
     },
   },
 };
